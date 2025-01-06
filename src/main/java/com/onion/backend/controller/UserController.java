c9f8a1c0f74c1fe5956aa757591827603b4c4f04
freelife1191 <freelife1191.good@gmail.com>
2025-01-06 23:00:14 +0900

    Part1 - Ch3.08 JWT 기반 로그아웃 기능 개발

--- src/main/java/com/onion/backend/controller/UserController.java
+++ src/main/java/com/onion/backend/controller/UserController.java
@@ -6,6 +6,8 @@ import com.onion.backend.jwt.JwtUtil;
 import com.onion.backend.service.CustomUserDetailsService;
 import com.onion.backend.service.UserService;
 import io.swagger.v3.oas.annotations.Parameter;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -42,24 +44,47 @@ public class UserController {
 
     @DeleteMapping("/{userId}")
     public ResponseEntity<Void> deleteUser(
-            @Parameter(description = "ID of the user to be deleted", required = true) @PathVariable Long userId) {
+            @Parameter(description = "ID of the user to be deleted", required = true)
+            @PathVariable("userId") Long userId) {
         userService.deleteUser(userId);
         return ResponseEntity.noContent().build();
     }
 
     @PostMapping("/login")
-    public String login(@RequestParam String username, @RequestParam String password) throws AuthenticationException {
+    public String login(
+            @RequestParam("username") String username,
+            @RequestParam("password") String password,
+            HttpServletResponse response
+            ) throws AuthenticationException {
         // Spring Security에서 제공하는 AuthenticationManager를 사용하여 인증을 수행
         // 인증에 성공하면 UserDetails를 반환하고, 실패하면 AuthenticationException을 발생
         authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
         UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-        // JWT 토큰 생성
-        return jwtUtil.generateToken(userDetails.getUsername());
+        // JWT 토큰 생성 후 응답
+        // return jwtUtil.generateToken(userDetails.getUsername());
+
+        String token = jwtUtil.generateToken(userDetails.getUsername());
+        Cookie cookie = new Cookie("onion_token", token);
+        cookie.setHttpOnly(true);
+        cookie.setPath("/");
+        cookie.setMaxAge(60 * 60);
+
+        response.addCookie(cookie);
+        return token;
+    }
+
+    @PostMapping("/logout")
+    public void logout(HttpServletResponse response) {
+        Cookie cookie = new Cookie("onion_token", null);
+        cookie.setHttpOnly(true);
+        cookie.setPath("/");
+        cookie.setMaxAge(0); // 쿠키 삭제
+        response.addCookie(cookie);
     }
 
     @PostMapping("/token/validation")
     @ResponseStatus(HttpStatus.OK)
-    public void jwtValidate(@RequestParam String token) {
+    public void jwtValidate(@RequestParam("token") String token) {
         if (!jwtUtil.validateToken(token)) {
             throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Token is not validation");
         }
